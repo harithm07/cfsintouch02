f77df18aa914ddfb10164a4ab55f3bfe3789aa2d
Amirul Ikhwan <amirikhwanfaisal@gmail.com>
2026-04-18 16:55:58 +0800

    feature/theme

---
+++ theme.js
@@ -0,0 +1,19 @@
+const themeToggle = document.getElementById("themeToggle");
+
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+    themeToggle.textContent = "☀️ Light Mode";
+}
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+        themeToggle.textContent = "☀️ Light Mode";
+    } else {
+        localStorage.setItem("theme", "light");
+        themeToggle.textContent = "🌙 Dark Mode";
+    }
+});
